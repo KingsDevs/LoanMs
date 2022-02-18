4310a0026764bc63a4e36bf9a225607b70910c18
KingsDevs <sunnyckair@gmail.com>
2022-02-19 04:44:31 +0800

    add coop member

---
+++ src/objects/CoopMember.java
@@ -0,0 +1,35 @@
+package objects;
+
+public class CoopMember 
+{
+    private String firstname;    
+    private String middlename;    
+    private String lastname;    
+    private String position;    
+    private String address;
+    
+    public String getFirstname()
+    {
+        return firstname;
+    }
+
+    public String getMiddleName()
+    {
+        return middlename;
+    }
+
+    public String getLastName()
+    {
+        return lastname;
+    }
+
+    public String getPosition()
+    {
+        return position;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+}
